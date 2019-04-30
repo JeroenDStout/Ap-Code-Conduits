e5d97b60d561a39b4566e76b18253caa5b420cd2
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-30 16:28:40 +0200

    Simplify await message and disposable message

--- Pubc/Await Message.h
+++ Pubc/Await Message.h
@@ -8,19 +8,36 @@
 #include <condition_variable>
 
 #include "Conduits/Pubc/Base Message.h"
+#include "Conduits/Pubc/Interface Conduit.h"
 
 namespace Conduits {
 
     class BaseAwaitMessage : public IBaseMessage {
     protected:
-        bool                     Has_Been_Released;
         std::mutex               Mx_Condition_Variable;
         std::condition_variable  Cv_Condition_Variable;
+        bool                     Has_Been_Released;
 
     public:
-        void sender_await() noexcept;
+        void sender_await() noexcept {
+            std::unique_lock<std::mutex> lk(this->Mx_Condition_Variable);
+            this->Cv_Condition_Variable.wait(lk, [&]{
+                return this->Has_Been_Released;
+            });
+        }
+        
+            // We (for now) do not allow an await message to open a conduit
+        void open_conduit_for_sender(Raw::INexus*, Raw::IOpenConduitHandler * handler) noexcept override {
+            Raw::IOpenConduitHandler::ResultData data;
+            handler->handle_failure(&data);
+        }
 
-        void release() noexcept override;
+        void release() noexcept override {
+            std::unique_lock<std::mutex> lk(this->Mx_Condition_Variable);
+            this->Has_Been_Released = true;
+            this->Cv_Condition_Variable.notify_all();
+        }
     };
+    
 
 }
